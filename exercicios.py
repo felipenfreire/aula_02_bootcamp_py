6b88b8dffbc0488c4cd28c2f2403be636e420a70
Felipe do Nascimento Freire <felipenascimento871@gmail.com>
2026-02-03 15:23:29 -0300

    adicionado exercício 03

--- exercicios.py
+++ exercicios.py
@@ -10,7 +10,12 @@ print(soma)
 numero_usuario = int(input('Digite uma número: '))
 resto_divisao = numero_usuario % 5
 print(resto_divisao)
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+numero1 = int(input('Digite um número: '))
+numero2 = int(input('digite outro númento: '))
+resultado = numero1 * numero2
+print(resultado)
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
