6ff52f95cf01dce4b60a59eae03dd29a711f3f13
Felipe do Nascimento Freire <felipenascimento871@gmail.com>
2026-02-03 19:25:05 -0300

    adicionado exercício 11

--- exercicios.py
+++ exercicios.py
@@ -49,12 +49,16 @@ base = float(input('Qual a base: '))
 potencia = float(input('qual a potência: '))
 resultado3 = base ** potencia
 print(resultado3)
+
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+nome_usuario = input('Qual seu nome de usuário: ')
+print(nome_usuario.upper())
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
